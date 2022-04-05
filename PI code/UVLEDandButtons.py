2d2766aaf7b5822211e1491463d020bf5fa133f3
josephpacifici7 <61980242+josephpacifici7@users.noreply.github.com>
2022-04-05 18:18:42 -0400

    Update UVLEDandButtons.py

--- PI code/UVLEDandButtons.py
+++ PI code/UVLEDandButtons.py
@@ -3,8 +3,11 @@ import os
 import sys
 import time
 import RPi.GPIO as GPIO
+import pygame
+from picamera import PiCamera
 
 # ================== FUNCTIONS ===================== #
+
 def check_debug():       
     while(True):
         if(GPIO.input(debugButton) == 0):
@@ -17,18 +20,42 @@ def check_lights():
         if(GPIO.input(lightButton) == 0):
             if lightOn == False:
                 GPIO.output(lightPin1,0)
-                GPIO.output(lightPin2,0)
                 lightOn = True;
                 time.sleep(0.15)
             else:
                 GPIO.output(lightPin1,1)
-                GPIO.output(lightPin2,1)
                 lightOn = False;
                 time.sleep(0.15)
 
-def run_camera():
-#     resolution of lcd screen is 800x400
-    os.system('libcamera-vid -t 15000 --width 800 --height 400 --fullscreen')        
+# def check_touchscreen():
+    # global running 
+    
+    # while(True):
+        # if(running):
+            # pygame.time.delay(10)
+            # for event in pygame.event.get():
+                # if event.type == pygame.QUIT:
+                    # running = False 
+                    
+            # # detects single tap from touchscreen              
+            # if event.type==pygame.MOUSEBUTTONDOWN:
+                
+                # # capture image 
+                # camera.capture('/home/pi/Desktop/images/image.jpg')
+                
+                # # stop preview
+                # camera.stop_preview()
+                
+                # # show image fullscreen using feh
+                # os.system("feh -F --on-last-slide quit /home/pi/Desktop/images")
+                
+                # #restart preview behind the image
+                # #after screen tap the video feed will already be loaded
+                # camera.start_preview()   
+            
+    
+# def run_camera(camera):
+    
     
 def setup_background_threads():
     light = threading.Thread(target=check_lights, daemon=True)
@@ -36,6 +63,10 @@ def setup_background_threads():
 
     debug = threading.Thread(target=check_debug, daemon=True)
     debug.start()
+    
+    #touchscreen = threading.Thread(target=check_touchscreen, daemon=True)
+    
+   #touchscreen.start()
 
     
 # ================== DEFINITIONS ===================== # 
@@ -52,26 +83,66 @@ debugButton = 26
 
 exit_program = 0
 
+# Loop condition 
+global running 
+running = False 
+
+
+
+# GPIO INIT 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(lightPin1, GPIO.OUT)
+
+GPIO.setup(lightButton, GPIO.IN)
+GPIO.setup(debugButton, GPIO.IN)
+
+GPIO.output(lightPin1, 0)
+
+# CAMERA INIT 
+camera = PiCamera()
+camera.brightness = 70
+
+# TOUCHSCREEN INIT
+pygame.init()
+window = pygame.display.set_mode((800,400))
+pygame.display.set_caption("Touchscreen Detection")
+
+
 # ================== MAIN ===================== # 
-if __name__ == "__main__":
-    GPIO.setmode(GPIO.BCM)
-    # Set the pins to inputs or outputs 
-    GPIO.setup(lightPin1, GPIO.OUT)
-    GPIO.setup(lightPin2, GPIO.OUT)
-    GPIO.setup(button_vcc, GPIO.OUT)
+# THREAD STARTS  
+setup_background_threads()  
 
-    GPIO.setup(lightButton, GPIO.IN)
-    GPIO.setup(debugButton, GPIO.IN)
+camera.start_preview()
 
-    GPIO.output(lightPin1, 0)
-    GPIO.output(lightPin2, 0)
-    GPIO.output(button_vcc, 1)
+running = True
 
-    setup_background_threads()
-    
+while(running):       
+    pygame.time.delay(10)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False 
+            
+    # detects single tap from touchscreen              
+    if event.type==pygame.MOUSEMOTION:
         
-    camera = threading.Thread(target=run_camera)
-    camera.start()
-    
-    while(True):
-        pass
+        # capture image 
+        camera.capture('/home/pi/Desktop/images/image.jpg')
+        
+        # stop preview
+        camera.stop_preview()
+        
+        # show image fullscreen using feh
+        os.system("feh -F --on-last-slide quit /home/pi/Desktop/images")
+        
+        #restart preview behind the image
+        #after screen tap the video feed will already be loaded
+        camera.start_preview()
+
+pygame.quit()            
+        
+
+
+
+
+
+
